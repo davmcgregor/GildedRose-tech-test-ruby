097ae3a1d53194db1bdc308f54e6334e5197214e
davidmcgregor <davidjohnmcgregor@gmail.com>
2020-03-24 23:11:49 +0000

    Add tests for 'Sulfuras' item

--- spec/gilded_rose_spec.rb
+++ spec/gilded_rose_spec.rb
@@ -49,5 +49,20 @@ describe GildedRose do
         expect(items[0].quality).to eq 50
       end
     end
+
+    context "when the item is 'Sulfuras, Hand of Ragnaros'" do
+      it "does not change the SellIn value" do
+        items = [Item.new("Sulfuras, Hand of Ragnaros", 10, 10)]
+        GildedRose.new(items).update_quality()
+        expect(items[0].sell_in).to eq 10
+      end
+
+      it "does not change the Quality value" do
+        items = [Item.new("Sulfuras, Hand of Ragnaros", 10, 10)]
+        GildedRose.new(items).update_quality()
+        expect(items[0].quality).to eq 10
+      end
+    end
+
   end
 end
