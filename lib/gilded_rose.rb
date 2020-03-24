03a42608749bd593697b2f48061e838e70154cce
davidmcgregor <davidjohnmcgregor@gmail.com>
2020-03-24 22:33:13 +0000

    Add tests for #update_quality method

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -31,9 +31,11 @@ class GildedRose
           end
         end
       end
+
       if item.name != "Sulfuras, Hand of Ragnaros"
         item.sell_in = item.sell_in - 1
       end
+
       if item.sell_in < 0
         if item.name != "Aged Brie"
           if item.name != "Backstage passes to a TAFKAL80ETC concert"
@@ -51,6 +53,7 @@ class GildedRose
           end
         end
       end
+      
     end
   end
 end
--- spec/gilded_rose_spec.rb
+++ spec/gilded_rose_spec.rb
@@ -4,10 +4,38 @@ describe GildedRose do
 
   describe "#update_quality" do
     it "does not change the name" do
-      items = [Item.new("foo", 0, 0)]
+      items = [Item.new("test_item", 10, 10)]
       GildedRose.new(items).update_quality()
-      expect(items[0].name).to eq "foo"
+      expect(items[0].name).to eq "test_item"
     end
+
+    it "lowers the SellIn value by 1 at the end of each day" do
+      items = [Item.new("test_item", 10, 10)]
+      GildedRose.new(items).update_quality()
+      expect(items[0].sell_in).to eq 9
+    end
+
+    it "lowers the Quality value by 1 at the end of each day" do
+      items = [Item.new("test_item", 10, 10)]
+      GildedRose.new(items).update_quality()
+      expect(items[0].quality).to eq 9
+    end
+
+    it "degrades the Quality valie twice as fast once the sell by date has passed" do
+      items = [Item.new("test_item", 0, 10)]
+      GildedRose.new(items).update_quality()
+      expect(items[0].quality).to eq 8
+    end
+
+    it "does not lower the Quality value below 0" do
+      items = [Item.new("test_item", 10, 10)]
+      gilded_rose = GildedRose.new(items)
+      15.times do
+        gilded_rose.update_quality()
+      end
+      expect(items[0].quality).to eq 0
+    end
+
   end
 
 end
